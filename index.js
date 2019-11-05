84cb4ab88dd21603d6acec907953692d9e5382a1
robyafrizal <robyafrizal86@gmail.com>
2019-11-05 15:00:25 +0700

    Add Project Javascript Shape Console

---
+++ index.js
@@ -0,0 +1,8 @@
+const rectangleWidth = 100;
+const rectangleLength = 20;
+
+const rectangleArea = rectangleWidth * rectangleLength;
+
+console.log(
+  `Luas dari Persegi dengan lebar ${rectangleWidth} dan panjang ${rectangleLength} adalah ${rectangleArea}`
+);
